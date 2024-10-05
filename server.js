f0a2528c564fc3a727bf99a128b2214e4c732f02
Bytebeem <donald.mxolisi@proton.me>
2024-10-05 16:50:38 +0200

    new

--- server.js
+++ server.js
@@ -27,14 +27,14 @@ app.use(express.json())
 app.use(cors())
 
 
-/*
-app.use('/api/personal-info', personalInfoRoutes);
-app.use('/api/education', educationRoutes);
-app.use('/api/work-experience', workExperienceRoutes);
-app.use('/api/skills', skillsRoutes);
-app.use('/api/projects', projectsRoutes);
-app.use('/api/contact', contactRoutes);
-*/
+
+app.use('/personal-info', personalInfoRoutes);
+app.use('/education', educationRoutes);
+app.use('/work-experience', workExperienceRoutes);
+app.use('/skills', skillsRoutes);
+app.use('/projects', projectsRoutes);
+app.use('/contact', contactRoutes);
+
 
 
 app.get("/",(req,res)=>{
